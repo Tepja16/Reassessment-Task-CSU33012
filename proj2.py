a4ef9b250cc305c896dbea338c86d3b5adff8dfb
Conor Sheridan <56885173+Tepja16@users.noreply.github.com>
2022-08-22 13:54:38 -0400

    Update proj2.py
    Restructuring.
    
    Changed to accept API key via command line argument.

--- proj2.py
+++ proj2.py
@@ -5,22 +5,7 @@ import numpy as np
 
 import time
 
-#y = np.array([35, 25, 25, 15])
-#mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
-
-#plt.pie(y, labels = mylabels, startangle = 90)
-#plt.show() 
-
-# First create a Github instance:
-
-# using an access token
-g = Github("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")
-
-#g = Github()
-user = g.get_user("d3")
-repo = user.get_repo("d3") 
-
-
+import sys
 
 def show_chart(date, labels, data):
 	plt.title(f"Additions made during the peroid of year: {date[0]}, month: {date[1]}")
@@ -75,38 +60,19 @@ def get_chart(repo):
 			month_data[commit.commit.author.name][0] = month_data[commit.commit.author.name][0] + commit.stats.additions
 			month_data[commit.commit.author.name][1] = month_data[commit.commit.author.name][1] + commit.stats.deletions
 			
+if __name__ == "__main__":
+
+	if len(sys.argv) > 1:
+		api_token = sys.argv[1] 
+	else:
+		api_token = ""
 
 
+	g = Github(api_token)
 
-#			print(f"The current year is: {current_year}")
-#			print(f"commit author: {commit.commit.author.name}")
-#			print(f"commit committer: {commit.commit.committer.name}")
+	user = g.get_user("d3")
+	repo = user.get_repo("d3") 
 
-			#if
-#			print(f"commit date: y: {commit.commit.author.date.year}, m: {commit.commit.author.date.month}")
+	get_chart(repo)
 	
-
-def list_commits(repo):
-
-	i = 0
-	for commit in repo.get_commits():
-		print(f"no : {i}")
-		#print(f"commit sha: {commit.sha}")
-		print(f"commit author: {commit.commit.author.name}")
-		#print(f"commit committer: {commit.commit.committer.name}")
-		print(f"commit date: y: {commit.commit.author.date.year}, m: {commit.commit.author.date.month}")
-		print(f"commit code additions: {commit.stats.additions}")
-		print(f"commit code deletions: {commit.stats.deletions}")
-		#print(f"no {i}: {commit.commit.author.name}  on  {commit.commit.author.date}, additions: {commit.stats.additions}")
-		print()
-		i = i+1
-
-
-get_chart(repo)
-
-
-
-
-
-
 
