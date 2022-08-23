367a7e1103dd6896b0680e60a8c2897df65f1216
Conor Sheridan <56885173+Tepja16@users.noreply.github.com>
2022-08-23 18:02:22 -0400

    Update proj2.py
    Fixed a problem with the month_data problem. Progress on making a yearly chart window

--- proj2.py
+++ proj2.py
@@ -1,5 +1,6 @@
 from github import Github
 
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,61 +8,105 @@ import time
 
 import sys
 
+
+def parse_month_data(month_data):
+	
+	labels = [] 
+	data = []	
+
+	for key, val in month_data.items():
+		#date = (val["year"], val["month"])
+		labels.append(key)
+		data.append(val["additions"])
+
+	date = (
+		month_data[labels[0]]["year"],
+		month_data[labels[0]]["month"]
+	)
+
+	return date, labels, data
+
+
+def show_year_charts(year_data):
+	#fig, axes = plt.subplots(1,len(year_data), figsize=(5, 5))
+
+	#i = 0
+	for month_data in year_data:
+
+		date, labels, data = parse_month_data(month_data)
+
+		print(date, labels, data)
+		#axes[i].title(f"Additions made during the peroid of year: {date[0]}, month: {date[1]}")
+		#axes[i].pie(data, labels = labels, startangle = 90, autopct='%.0f%%')
+
+		#i = i + 1
+	
+	print()
+	#plt.show()
+
+
+
+
 def show_chart(date, labels, data):
 	plt.title(f"Additions made during the peroid of year: {date[0]}, month: {date[1]}")
+	
+		
 	plt.pie(data, labels = labels, startangle = 90, autopct='%.0f%%')
 	plt.show()
 
 
+
+
 def get_chart(repo):
 
+	# Set the initial year and month
 	current_year = repo.get_commits()[0].commit.author.date.year
 	current_month = repo.get_commits()[0].commit.author.date.month
 
 	month_data = {}
+	year_data = []
 	for commit in repo.get_commits():
 		
-
-		if commit.commit.author.date.year != current_year:
-			current_year = commit.commit.author.date.year
-	
-		if commit.commit.author.date.month != current_month:
-			print(f"current_data: {month_data}")
-			print(list(month_data.values()))
-			print([i[0] for i in list(month_data.values())])
-			print(list(month_data.keys()))
+		# Make a new month_data dictionary
+		if current_month != commit.commit.author.date.month or (current_month == commit.commit.author.date.month and current_year != commit.commit.author.date.year):
 			
-			show_chart(
-				(current_year, current_month),
-				list(month_data.keys()), 
-				[i[0] for i in list(month_data.values())]
-			)
+			# Append the current month_data to the year data list			
+			year_data.append(month_data)
 			
-			
+			# Update the month index and empty the current month_data dictionary to prepare making a new month_data dictionary
 			current_month = commit.commit.author.date.month 
 			month_data = {}
-			#time.sleep(1)
-			
-			
 
+		# Make a new year_data list
+		if current_year != commit.commit.author.date.year:
 
+			show_year_charts(year_data)
+			# Update the year index and empty the current year_data list to prepare making a new year_data list
+			current_year = commit.commit.author.date.year
+			year_data = []
+	
 
-
+		# Append new month commit data to the year_data list
 		if commit.commit.author.name not in month_data:
+
+			# Append the commit data to the month_data list, the data is of the form {<author> : {"additions" : <additions>, "deletions" : <deletions>, "year" : <year>, "month" : <month>}}
 			month_data.update({
-				commit.commit.author.name : [
-					commit.stats.additions,
-					commit.stats.deletions,
-					commit.commit.author.date.year,
-					commit.commit.author.date.month
-				]
+				commit.commit.author.name : {
+					"additions" : commit.stats.additions,
+					"deletions" : commit.stats.deletions,
+					"year" : commit.commit.author.date.year,
+					"month" : commit.commit.author.date.month
+				}
 			})
 		else:
-			month_data[commit.commit.author.name][0] = month_data[commit.commit.author.name][0] + commit.stats.additions
-			month_data[commit.commit.author.name][1] = month_data[commit.commit.author.name][1] + commit.stats.deletions
+
+			# If the authors name is already in the month_data dictionary for this month, add their additions and deletions together
+			month_data[commit.commit.author.name]["additions"] = month_data[commit.commit.author.name]["additions"] + commit.stats.additions
+			month_data[commit.commit.author.name]["deletions"] = month_data[commit.commit.author.name]["deletions"] + commit.stats.deletions
 			
 if __name__ == "__main__":
 
+	# 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
 	if len(sys.argv) > 1:
 		api_token = sys.argv[1] 
 	else:
